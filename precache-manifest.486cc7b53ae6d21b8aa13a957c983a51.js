b6638bcea21560b493247bd5d3d7271f711f1f56
unknown <hunter_xhunter39@hotmail.com>
2019-12-03 15:36:59 +0700

    Updates

---
+++ precache-manifest.486cc7b53ae6d21b8aa13a957c983a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0674cb1f040a915eb16792f574697839",
+    "url": "/react_js_from_pg_mosh/index.html"
+  },
+  {
+    "revision": "52ee5c5329f91a24a619",
+    "url": "/react_js_from_pg_mosh/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "d60f007a6398cb725183",
+    "url": "/react_js_from_pg_mosh/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "52ee5c5329f91a24a619",
+    "url": "/react_js_from_pg_mosh/static/js/2.58231fed.chunk.js"
+  },
+  {
+    "revision": "d60f007a6398cb725183",
+    "url": "/react_js_from_pg_mosh/static/js/main.69424206.chunk.js"
+  },
+  {
+    "revision": "6bf28f12f85cc38c1e9a",
+    "url": "/react_js_from_pg_mosh/static/js/runtime-main.ed38f408.js"
+  }
+]);
